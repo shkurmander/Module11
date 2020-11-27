bbcdfed8a4a4de5731e3a3562c317604ef3e1801
Vladimir Berdnikov <gazetter@mail.ru>
2020-11-27 20:05:35 +0700

    добавлено ex11.2.3

---
+++ Ex11.2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex11._2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Jane";
+            
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
