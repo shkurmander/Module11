e141c0cecad64143f2f7a62e1193e73600b596f3
Vladimir Berdnikov <gazetter@mail.ru>
2020-11-27 12:34:45 +0700

    Добавьте файлы проекта.

---
+++ Ex11.3.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex11._3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIAPet = true;
+            double MyShoeSize = 37.5;
+
+
+            Console.WriteLine($"My name is {MyName}");
+            Console.WriteLine($"My age is {MyAge}");
+            Console.WriteLine($"Do I have a pet? {HaveIAPet}");
+            Console.WriteLine($"My shoe size is  {MyShoeSize}");
+
+            Console.ReadKey();
+        }
+    }
+}
