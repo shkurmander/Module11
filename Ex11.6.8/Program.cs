fd0210274a4d4a424d2ed84155aa67358c22df9b
Vladimir Berdnikov <gazetter@mail.ru>
2020-11-30 18:42:06 +0700

    Add  Ex11.6.8 & 11.7.4

---
+++ Ex11.6.8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ex11._6._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 5 % 6;  
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ex11.7.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ex11._7._4
+{
+    class Program
+    {
+        enum DayOfWeek
+        {
+            Monday = 1,
+            Tuesday =2,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            var age = Console.ReadLine();
+            Console.WriteLine($"Your name is {name} and age is {age}");
+            Console.Write("What is your favorite day of week? ");
+
+            DayOfWeek favoriteDay = (DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(Convert.ToInt32(Console.ReadLine())-1);
+            Console.WriteLine($"Your favorite day is {favoriteDay} ");
+
+            Console.ReadKey();
+        }
+    }
+}
